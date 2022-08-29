62aa9890a9f7f27863d729f1b2c7eba83d569b3d
Kamal Hussein <kamalinuxx@gmail.com>
2022-08-29 14:44:42 -0700

    Added lists.h, linked_lists.c, 13-main.c 13-is_palindrome.c, palindrome files

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,7 +3,7 @@
 /**
  * reverse - reverses the second half of the list
  *
- * @h_r: head of the second half
+ * @head: head of the second half
  * Return: no return
  */
 void reverse(listint_t **head)
