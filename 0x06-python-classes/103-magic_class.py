e6592d9ff6331678d5967ecf15b61afb9812bd6d
Kamal Hussein <kamalinuxx@gmail.com>
2022-09-14 14:48:50 -0700

    Adding 103-magic_class.py file

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import math
+
+
+class MagicClass:
+    """Represent a circle."""
+
+    def __int__(self, redius=0):
+        """Initialize a MagicClass."""
+
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        self.__radius = radius
+
+        def area(self):
+            """Method that calculates the area of the circumference."""
+            return ((self.radius ** 2) * math.pi)
+
+        def circumference(self):
+            """Method that calculates the perimeter of a circumference."""
+            return (2 * math.pi * self.__radius)
