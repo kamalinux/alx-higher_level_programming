ba9940185ad9f776970888a42c0b4481df31df6a
Kamal Hussein <kamalinuxx@gmail.com>
2022-09-14 14:59:20 -0700

    Adding 103-magic_class.py file

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -16,10 +16,10 @@ class MagicClass:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
-        def area(self):
-            """Method that calculates the area of the circumference."""
-            return ((self.__radius ** 2) * math.pi)
+    def area(self):
+        """Method that calculates the area of the circumference."""
+        return ((self.__radius ** 2) * math.pi)
 
-        def circumference(self):
-            """Method that calculates the perimeter of a circumference."""
-            return (2 * math.pi * self.__radius)
+    def circumference(self):
+        """Method that calculates the perimeter of a circumference."""
+        return (2 * math.pi * self.__radius)
