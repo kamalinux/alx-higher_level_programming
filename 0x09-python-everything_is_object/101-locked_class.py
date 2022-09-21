e242d75859f99b43207437d471a6c51bc5c61459
Kamal Hussein <kamalinuxx@gmail.com>
2022-09-21 11:24:41 -0700

    Adding 101-main.py, 101-locked_class.py files

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,4 +6,9 @@ dynmaically created attributes
 
 
 class LockedClass:
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
+
     __slots__ = ['first_name']
