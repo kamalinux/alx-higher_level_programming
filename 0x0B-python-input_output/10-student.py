bd9e34df46528580ed24a49cf15573f71cfe100d
Kamal Hussein <kamalinuxx@gmail.com>
2022-09-27 18:16:42 -0700

    Adding class Student that defines a student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Module that defines the class Student
+"""
+
+
+class Student:
+    """ Class to create student instances """
+
+    def __init__(self, first_name, last_name, age):
+        """ Special method to initialize """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Method that returns directory description """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
