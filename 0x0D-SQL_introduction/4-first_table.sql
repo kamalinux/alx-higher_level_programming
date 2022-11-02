70abbaccaa14116e6cdfb05d96848131daa69b85
Kamal Hussein <kamalinuxx@gmail.com>
2022-11-01 18:27:32 -0700

    Adding a script that creates a table  in the current database

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Creates a table called first_table in the current db
+-- CREATE table IF NOT EXISTS first_table(`id` INT, `name` VARCHAR(256));
+
+CREATE TABLE IF NOT EXISTS first_table (
+id INT,
+name VARCHAR(256));
