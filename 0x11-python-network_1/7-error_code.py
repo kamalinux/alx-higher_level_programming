a758bb92a99fab50f36456fb6853a9a0ac3e4ff4
Kamal Hussein <kamalinuxx@gmail.com>
2022-12-16 14:20:17 -0800

    Adding a Python script that takes in a URL, sends a request to the URL and displays the body of the response.

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL
+and displays the body of the response.
+Usage: ./7-error_code.py http://0.0.0.0:5000/status_401
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get(argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
